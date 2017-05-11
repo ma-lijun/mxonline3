1f3c7bfddc2be088fd8034cbe7ec87d34c6aa019
ma-lijun <343190282@qq.com>
2017-05-12 00:39:42 +0800

    写到6-2，没有写好是否登录成功的 if else ，没有写好登录逻辑

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 # Create your views here.
+def login(request):
+    if request.method=='POST':
+        user_name=request.POST.get('username','')
+        pass_word = request.POST.get('password', '')
+        user=authenticate(username=user_name,password=pass_word)
+        if user is not None:
+            login(request,user)
+            render(request,'index.html')
+        else:
+            render(request,'login.html',{})
+    elif request.method=='GET':
+        return render(request,'login.html',{})
