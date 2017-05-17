5a3d8b4e0f867fa9d341fc91eea881cb63417ef4
ma-lijun <343190282@qq.com>
2017-05-17 19:19:16 +0800

    完成新用户注册邮件发送，但是没有链接

--- apps/users/views.py
+++ apps/users/views.py
@@ -5,9 +5,11 @@ from django.contrib.auth.backends import ModelBackend
 from django.db.models import Q
 from django.views.generic.base import View
 from django import forms
+from django.contrib.auth.hashers import make_password
 
 from .models import UserProfile
-from .forms import LoginForms
+from .forms import LoginForms,RegisterForm
+from utils.email_send import send_register_email
 
 class CustomBackend(ModelBackend):
     def authenticate(self, username=None, password=None, **kwargs):
@@ -39,16 +41,25 @@ class LoginView(View):
         else:
             return render(request, 'login.html', {'login_form':login_form})
 
-# 基于函数的方式不实用，以后都用基于类的方式
-# def user_login(request):
-#     if request.method=='POST':
-#         user_name=request.POST.get('username','')
-#         pass_word = request.POST.get('password', '')
-#         user=authenticate(username=user_name,password=pass_word)
-#         if user is not None:
-#             login(request, user)
-#             return render(request, 'index.html')
-#         else:
-#             return render(request, 'login.html', {'msg':'用户名或密码错误！'})
-#     elif request.method=='GET':
-#         return render(request, 'login.html', {})
+
+class RegisterView(View):
+    def get(self,request):
+        register_form=RegisterForm()
+        return render(request, 'register.html', {'register_form':register_form})
+
+    def post(self,request):
+        register_form=RegisterForm(request.POST)
+        if register_form.is_valid():
+            user_name=request.POST.get('email','')
+            pass_word=request.POST.get('password','')
+            user_profile=UserProfile()
+            user_profile.username=user_name
+            user_profile.email = user_name
+            user_profile.password=make_password(pass_word)
+            user_profile.save()
+
+            send_register_email(user_name,'register')
+            return render(request, 'login.html')
+        else:
+            return render(request, 'register.html')
+
